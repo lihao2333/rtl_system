c6f4085c983fde6d2f7a1cb36024674ded86c762
Your Name <you@example.com>
2018-12-30 17:10:07 +0800

    add get_test_time to jiasu dev stop

--- server/sender.py
+++ server/sender.py
@@ -1,9 +1,21 @@
 from SocketBox.socketSP import socketPub
+import datetime
 import time
 HOST = "0.0.0.0"
 PORT = 50000
+DELAY_SECOND = 5
+def get_test_time(delay_second):
+    now = datetime.datetime.now()
+    return "%02d%02d%02d"%(now.hour, now.minute, now.second+delay_second)
 TO_SEND = {
-        "time":"163000"
+        "time":get_test_time(DELAY_SECOND),
+        "params":{
+            "sample_rate":2.048e6 ,
+            "center_freq":70e6    ,
+            "freq_correction":60  ,
+            "gain":'auto',
+            "sample_num":512
+            }
         }
 with socketPub(HOST, PORT) as pub:
     pub.publish(TO_SEND, type="pyobj")
