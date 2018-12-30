eecc7f379ce762fc545d3cf99fed7a6622a1bc41
lihao <lihao2333.com@gmail.com>
2018-12-30 16:54:41 +0800

    add sdr to client

--- client/listener.py
+++ client/listener.py
@@ -1,8 +1,14 @@
 from SocketBox.socketSP import socketPub, socketSub
 from gps.gps import wait_time
+from sdr.MySdr import MySdr
 SERVER_HOST = "10.112.254.131"
 SERVER_PORT = 50000
 with socketSub([(SERVER_HOST, SERVER_PORT)]) as sub:
     for recvs in sub.subscribe(type="pyobj"):
         print("recv:",recvs[0])
+        my_sdr = MySdr()
+        my_sdr.set_paras(recvs[0]["params"])
         wait_time(recvs[0]["time"])
+        data = my_sdr.sample_data()
+        print(data)
+
