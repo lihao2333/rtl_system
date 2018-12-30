1f4a1f6b669b8e921acee5e389ff2fbcc1169395
lihao <lihao2333.com@gmail.com>
2018-12-30 16:27:56 +0800

    add gps

--- client/listener.py
+++ client/listener.py
@@ -2,5 +2,5 @@ from SocketBox.socketSP import socketPub, socketSub
 SERVER_HOST = "10.112.254.131"
 SERVER_PORT = 50000
 with socketSub([(SERVER_HOST, SERVER_PORT)]) as sub:
-    for recvs sub.subscribe():
-        print(recvs)
+    for recvs in sub.subscribe(type="pyobj"):
+        print(recvs[0])
---
+++ gps/gps.py
@@ -0,0 +1,15 @@
+import serial
+def wait_time(time):
+    with serial.Serial('/dev/ttyUSB0',9600) as ser:
+        while True:
+             datas = ser.readline().decode()
+             datas = datas.strip("\r\n").split(",")
+             if datas[0] == "$GNRMC":
+                 hour = "%02d"%(int(datas[1][:2])+8)
+                 minute = datas[1][2:4]
+                 second = datas[1][4:6]
+                 now = hour+minute+second
+                 print(now)
+                 if  time == now:
+                     break
+wait_time("162630")
