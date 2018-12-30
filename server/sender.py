4a4e296649dc50321eb5f0a1cdd59f23b19b251e
Your Name <you@example.com>
2018-12-30 16:30:27 +0800

    edit sender with TO_SEND and time

--- server/sender.py
+++ server/sender.py
@@ -2,9 +2,9 @@ from SocketBox.socketSP import socketPub
 import time
 HOST = "0.0.0.0"
 PORT = 50000
+TO_SEND = {
+        "time":"163000"
+        }
 with socketPub(HOST, PORT) as pub:
-    while True :
-        pub.publish(b"123")
-        print("123")
-        time.sleep(1)
+    pub.publish(TO_SEND, type="pyobj")
 
