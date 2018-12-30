a5856e7381c8d323e0e577d4968c75f10ac04bc9
lihao <lihao2333.com@gmail.com>
2018-12-30 17:19:48 +0800

    d

--- client/listener.py
+++ client/listener.py
@@ -4,9 +4,9 @@ from sdr.MySdr import MySdr
 SERVER_HOST = "10.112.254.131"
 SERVER_PORT = 50000
 with socketSub([(SERVER_HOST, SERVER_PORT)]) as sub:
+    my_sdr = MySdr()
     for recvs in sub.subscribe(type="pyobj"):
         print("recv:",recvs[0])
-        my_sdr = MySdr()
         my_sdr.set_paras(recvs[0]["params"])
         wait_time(recvs[0]["time"])
         data = my_sdr.sample_data()
