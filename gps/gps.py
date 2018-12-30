85e9c9669ce7be028ed498c2606e5642607a27ec
lihao <lihao2333.com@gmail.com>
2018-12-30 19:35:51 +0800

    edit try

--- gps/gps.py
+++ gps/gps.py
@@ -2,8 +2,8 @@ import serial
 def wait_time(time):
     with serial.Serial('/dev/ttyUSB0',9600) as ser:
         while True:
-             datas = ser.readline().decode()
              try:
+                 datas = ser.readline().decode()
                  datas = datas.strip("\r\n").split(",")
                  if datas[0] == "$GNRMC":
                      hour = "%02d"%(int(datas[1][:2])+8)
@@ -13,5 +13,5 @@ def wait_time(time):
                      print(now)
                      if  time == now:
                          break
-            except:
-                pass
+             except:
+                 pass
